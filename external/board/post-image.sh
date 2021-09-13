426cd6785ba6212d469e3564bae1628e94481d1b
takumin <takumiiinn@gmail.com>
2021-09-14 03:58:49 +0900

    support disk boot

--- external/board/post-image.sh
+++ external/board/post-image.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+set -eu
+
+BOARD_DIR="$(cd "$(dirname "$0")"; pwd)"
 START_QEMU_SCRIPT="${BINARIES_DIR}/start-qemu.sh"
+
+cp -f "${BOARD_DIR}/grub.cfg" "${BINARIES_DIR}/efi-part/EFI/BOOT/grub.cfg"
 
 declare -ar QEMU_CMD_LINE=(
 "qemu-system-aarch64"
@@ -8,18 +13,19 @@ declare -ar QEMU_CMD_LINE=(
 "-M" "virt"
 "-cpu" "cortex-a53"
 "-smp" "1"
-"-kernel" "Image"
-"-append" "'rootwait root=/dev/vda console=ttyAMA0'"
+"-bios" "QEMU_EFI.fd"
 "-netdev" "user,id=eth0"
 "-device" "virtio-net-device,netdev=eth0"
-"-drive" "file=rootfs.ext4,if=none,format=raw,id=hd0"
+"-drive" "file=disk.img,if=none,format=raw,id=hd0"
 "-device" "virtio-blk-device,drive=hd0"
+"-device" "virtio-rng-pci"
 )
 
 cat <<-_EOF_ > "${START_QEMU_SCRIPT}"
 	#!/bin/sh
 	(
-	BINARIES_DIR="\${0%/*}/"
+	set -eu
+	BINARIES_DIR="\$(cd "\$(dirname "\$0")"; pwd)"
 	cd "\${BINARIES_DIR}"
 	export PATH="${HOST_DIR}/bin:\${PATH}"
 	exec ${QEMU_CMD_LINE[@]}
